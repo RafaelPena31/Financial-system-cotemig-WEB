caf6264e7d63a9b162a956ad32770fb9674d8115
Rafael Pena <rafaelppena31@gmail.com>
2020-11-13 11:26:58 -0300

    config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 140,
+  tabWidth: 4,
+  useTabs: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  endOfLine: 'auto'
+}
