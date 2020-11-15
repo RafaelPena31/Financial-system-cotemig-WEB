52fcae0e380a7c46a8fb84b7cca32d0820242d3b
Rafael Pena <rafaelppena31@gmail.com>
2020-11-15 00:36:43 -0300

    Finish History Function Display

---
+++ Screens/History/History.js
@@ -0,0 +1,183 @@
+function ChangeMonth(month) {
+	switch (month) {
+		case 'jan':
+			document.getElementById('main-table-jan').style.display = 'block'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'fev':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'block'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'mar':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'block'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'abr':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'block'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'mai':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'block'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'jun':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'block'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'jul':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'block'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'ago':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'block'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'set':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'block'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'out':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'block'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'nov':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'block'
+			document.getElementById('main-table-dez').style.display = 'none'
+			break
+
+		case 'dez':
+			document.getElementById('main-table-jan').style.display = 'none'
+			document.getElementById('main-table-fev').style.display = 'none'
+			document.getElementById('main-table-mar').style.display = 'none'
+			document.getElementById('main-table-abr').style.display = 'none'
+			document.getElementById('main-table-mai').style.display = 'none'
+			document.getElementById('main-table-jun').style.display = 'none'
+			document.getElementById('main-table-jul').style.display = 'none'
+			document.getElementById('main-table-ago').style.display = 'none'
+			document.getElementById('main-table-set').style.display = 'none'
+			document.getElementById('main-table-out').style.display = 'none'
+			document.getElementById('main-table-nov').style.display = 'none'
+			document.getElementById('main-table-dez').style.display = 'block'
+			break
+	}
+}
