8532f0702e96f5d99296a3609a923763ce615aaa
Rafael Pena <rafaelppena31@gmail.com>
2020-11-18 00:43:31 -0300

    Progress on Registration Class

--- Classes/Registration.php
+++ Classes/Registration.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 require_once "Conexao.php";
 
 class Registration
@@ -9,24 +8,71 @@ class Registration
     public $value;
     public $categoryExpenseId;
     public $categoryRecipeId;
+    public $data;
     
-    public function CreateRegistration() {
-        try {
-            if(isset($_POST["valueDesp"]) && !empty($_POST["valueDesp"])) {
+    public function CreateRegistration($type) {
+            try {
+              if(isset($_POST["valueRegistration"]) && !empty($_POST["valueRegistration"]) && isset($_POST["typeRegistration"]) && !empty($_POST["typeRegistration"]) && isset($_POST["dateRegistration"]) && !empty($_POST["dateRegistration"])) {
 
-                $this->categoryId = $_POST["D"];
-                $this->value = $_POST["valueDesp"];
+                switch ($type) {
+                  case 'D':
 
-                $bd = new Conexao();
-                $con = $bd->conectar();
-                $sql = $con->prepare("insert into Registration(id,value,data,User_id,Category_id) values(null,?,?,?,?)");
+                    $this->categoryId = intval($_POST["valueRegistration"]);
+                    $this->value = doubleval(substr($_POST["valueRegistration"], 2));
+                    $this->data = $_POST["dateRegistration"];
+    
+                    $bd = new Conexao();
+                    $con = $bd->conectar();
+                    $sql = $con->prepare("insert into Registration(id,value,data,User_id,Category_id) values(null,?,?,?,?)");
+    
+                    $sql->execute(array(
+                        $this->value,
+                        $this->data,
+                        $_SESSION["userToken"],
+                        $this->categoryId
+                    ));
+                    var_dump($this->categoryId);
 
-                $sql->execute(array(
-                    $this->value,
-                    $_SESSION["userToken"],
-                    $this->categoryId
-                ));
+                    if($sql->rowCount() > 0) {
+                      echo "<script> alert('Despesa registrado com sucesso.'); </script>";
+                      header('location: TransactionPage.php');
+                    } else {
+                      echo "<script> alert('Não foi possível registrar sua despesa. {$_POST['dateRegistration']}'); </script>";
+                    }
+
+                    break;
+                  
+                  case 'R':
+
+                    $this->categoryId = $_POST["valueRegistration"];
+                    $this->value = $_POST["valueRegistration"];
+                    $this->data = $_POST["dateRegistration"];
+    
+                    $bd = new Conexao();
+                    $con = $bd->conectar();
+                    $sql = $con->prepare("insert into Registration(id,value,data,User_id,Category_id) values(null,?,?,?,?)");
+    
+                    $sql->execute(array(
+                        $this->value,
+                        $this->data,
+                        $_SESSION["userToken"],
+                        $this->categoryId
+                    ));
+
+                    if($sql->rowCount() > 0) {
+                      echo "<script> alert('Receita registrada com sucesso.'); </script>";
+                      header('location: TransactionPage.php');
+                    } else {
+                      echo "<script> alert('Não foi possível registrar sua receita.'); </script>";
+                    }
+
+                    break;
+                }
+
+
             }      
+          } catch(PDOException $msg) {
+            echo "<script> alert('Não foi possível criar a sua despesa: {$msg->getMessage()}'); </script>";
           }
         }
 }
