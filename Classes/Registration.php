3e142b2365c27ee5483fea2dfd69301d530073b5
Rafael Pena <rafaelppena31@gmail.com>
2020-11-19 01:16:14 -0300

    Progress on Registration Class

--- Classes/Registration.php
+++ Classes/Registration.php
@@ -12,15 +12,15 @@ class Registration
     
     public function CreateRegistration($type) {
             try {
-              if(isset($_POST["valueRegistration"]) && !empty($_POST["valueRegistration"]) && isset($_POST["typeRegistration"]) && !empty($_POST["typeRegistration"]) && isset($_POST["dateRegistration"]) && !empty($_POST["dateRegistration"])) {
-
+              if(isset($_POST["valueRegistration"]) && !empty($_POST["valueRegistration"]) && isset($_POST["classRegistration"]) && !empty($_POST["classRegistration"]) && isset($_POST["dateRegistration"]) && !empty($_POST["dateRegistration"])) {
+                
                 switch ($type) {
                   case 'D':
 
-                    $this->categoryId = intval($_POST["valueRegistration"]);
+                    $this->categoryId = intval($_POST["classRegistration"]);
                     $this->value = doubleval(substr($_POST["valueRegistration"], 2));
                     $this->data = $_POST["dateRegistration"];
-    
+                    var_dump($this->value);
                     $bd = new Conexao();
                     $con = $bd->conectar();
                     $sql = $con->prepare("insert into Registration(id,value,data,User_id,Category_id) values(null,?,?,?,?)");
@@ -31,10 +31,9 @@ class Registration
                         $_SESSION["userToken"],
                         $this->categoryId
                     ));
-                    var_dump($this->categoryId);
 
                     if($sql->rowCount() > 0) {
-                      echo "<script> alert('Despesa registrado com sucesso.'); </script>";
+                      echo "<script> alert('Despesa registrada com sucesso.'); </script>";
                       header('location: TransactionPage.php');
                     } else {
                       echo "<script> alert('Não foi possível registrar sua despesa.'); </script>";
@@ -44,14 +43,14 @@ class Registration
                   
                   case 'R':
 
-                    $this->categoryId = $_POST["valueRegistration"];
-                    $this->value = $_POST["valueRegistration"];
+                    $this->categoryId = intval($_POST["classRegistration"]);
+                    $this->value = doubleval(substr($_POST["valueRegistration"], 2));
                     $this->data = $_POST["dateRegistration"];
-    
+
                     $bd = new Conexao();
                     $con = $bd->conectar();
                     $sql = $con->prepare("insert into Registration(id,value,data,User_id,Category_id) values(null,?,?,?,?)");
-    
+                    
                     $sql->execute(array(
                         $this->value,
                         $this->data,
@@ -79,21 +78,21 @@ class Registration
 
 
 
-    public function ListingRegistration($type){
+    public function ListingRegistration($type, $month){
       try {
         $bd = new Conexao();
         $con = $bd->conectar();
-        $sql = $con->prepare("select * from Registration where type = ? and User_id = ? and Category_id = ?");
+        $sql = $con->prepare("select Registration.data, Registration.value, Category.name, Category.type, Category.class from Registration join Category on Category_id = Category.id and Registration.User_id = ? where Category.type = ?;");
 
-      $sql->execute(array(
-          $type,
-          $_SESSION["userToken"],
-          $this->categoryId
-      ));
+        $sql->execute(array(
+            $_SESSION["userToken"],
+            $type,
+            $month
+        ));
 
-      if ($sql->rowCount() > 0) {
-          return $result = $sql->fetchAll(PDO::FETCH_CLASS);
-      }
+        if ($sql->rowCount() > 0) {
+             return $result = $sql->fetchAll(PDO::FETCH_CLASS);
+        }
 
       } catch (PDOException $msg) {
         echo "<script> alert('Não foi possível listar suas despesas e suas receitas: {$msg->getMessage()}'); </script>";
